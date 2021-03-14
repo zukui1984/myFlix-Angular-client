54e2342a1cc0d9d374d79f151748f42597387969
zukui <71074389+zukui1984@users.noreply.github.com>
2021-03-14 10:36:38 +0100

    add div class for display and container on movie-card.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,32 +14,4 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
 export class AppComponent {
   title = 'myFlix-Angular-client';
 
-  constructor(public dialog: MatDialog) { }
-  // This is the function that will open the dialog when the signup button is clicked  
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-      // Assigning the dialog a width
-      width: '280px'
-    });
-  }
-
-  openUserLoginDialog(): void {
-    this.dialog.open(UserLoginFormComponent, {
-      // Assigning the dialog a width
-      width: '280px'
-    });
-  }
-
-  openMoviesDialog(): void {
-    this.dialog.open(MovieCardComponent, {
-      width: '500px'
-    });
-  }
-
-  openUserProfileDialog(): void {
-    this.dialog.open(UserProfileComponent, {
-      width: '300px'
-    });
-  }
-
 }
